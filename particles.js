ec5321482f4eefdbc78a2547933afeee2a5ff765
Kalash Jindal <37014842+erickeagle@users.noreply.github.com>
2020-05-21 22:49:04 +0530

    Update particles.js

--- particles.js
+++ particles.js
@@ -1,11 +1,4 @@
-/* -----------------------------------------------
-/* Author : Vincent Garreau  - vincentgarreau.com
-/* MIT license: http://opensource.org/licenses/MIT
-/* Demo / Generator : vincentgarreau.com/particles.js
-/* GitHub : github.com/VincentGarreau/particles.js
-/* How to use? : Check the GitHub README
-/* v2.0.0
-/* ----------------------------------------------- */
+
 
 var pJS = function(tag_id, params){
 
